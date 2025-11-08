063f459be1e09b9b4aec6a7a68b99aec02a0db5b
veenzent <odumevincent19@gmail.com>
2025-11-08 07:36:12 +0100

    Director update added

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -24,3 +24,29 @@ console.log(teacher3);
 // fullTimeEmployee: false
 // lastName: "Doe"
 // location: "London"
+
+
+
+
+// write an interface names Directors that extends Teacher
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+  
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
+
+// should print
+// Object
+// firstName: "John"
+// fullTimeEmployee: true
+// lastName: "Doe"
+// location: "London"
+// numberOfReports: 17
