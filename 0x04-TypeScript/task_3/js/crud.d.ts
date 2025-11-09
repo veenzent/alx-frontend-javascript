89331cd4078c9fcae54229201f6de4f0f93e0ccb
veenzent <odumevincent19@gmail.com>
2025-11-09 17:00:09 +0100

    task 3 in progress

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,7 +1,7 @@
 import { RowID, RowElement } from "./interface.js";
 
 // Declare the functions that exist in crud.js
-declare function insertRow(row: RowElement): RowID;
+declare function insertRow(row: RowElement): number;
 declare function deleteRow(rowId: RowID): void;
 declare function updateRow(rowId: RowID, row: RowElement): RowID;
 
