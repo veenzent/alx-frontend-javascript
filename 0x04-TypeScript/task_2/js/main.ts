ec5d7715f40491b32d0a67541f7ebec6054772dd
veenzent <odumevincent19@gmail.com>
2025-11-09 13:07:04 +0100

    advanced types part 1 done

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,28 @@
+// Task 5. Advanced types Part 1
+
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+
+// Director class implementing Director Interface
+class Director implements DirectorInterface {
+    workFromHome(): string {
+        return "Working from home";
+    }
+
+    getCoffeeBreak(): string {
+        return "Getting a coffee break";
+    }
+
+    workDirectorTasks(): string {
+        return "Getting to director tasks";
+    }
+}
